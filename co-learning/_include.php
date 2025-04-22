8e8246e674501c097a1c24d4a2ba742e47038902
Imran Ali Namazi <imran@amadeusweb.com>
2025-04-22 21:03:19 +0530

    more co-learning old stuff

---
+++ co-learning/_include.php
@@ -0,0 +1,8 @@
+<?php
+DEFINE('NODEPATH', __DIR__);
+
+variables([
+	'nodeSiteName' => 'Integral Living &rarr; <span>Immersive</span> Learning',
+	'nodeSafeName' => 'aww-integral-living',
+	'submenu-at-node' => true,
+]);
---
+++ sunlight/_include.php
@@ -0,0 +1,8 @@
+<?php
+DEFINE('NODEPATH', __DIR__);
+
+variables([
+	'nodeSiteName' => 'A World <span>Union</span> and <span>Integral</span> Living',
+	'nodeSafeName' => 'aww-integral-living',
+	'submenu-at-node' => true,
+]);
